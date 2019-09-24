8af084aa330b0a6a4ead538930fec6964a782663
Minkyo Choi <eunho0815@icloud.com>
2019-09-25 01:37:29 +0900

    sans

--- wasabi/assets/js/mobile.js
+++ wasabi/assets/js/mobile.js
@@ -1,6 +1,6 @@
 var filter = "win16|win32|win64|mac|macintel";
 if( navigator.platform  ){
         if( filter.indexOf(navigator.platform.toLowerCase())<0 ){
-             location.href = "https://knmango.github.io";
+             location.href = "https://knmango.github.io/wasabi/main_mobile";
         }
  }
