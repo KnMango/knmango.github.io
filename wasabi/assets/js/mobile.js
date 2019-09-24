764b7e41d3260f0502b1a79401a1b5c113288b34
Minkyo Choi <eunho0815@icloud.com>
2019-09-25 01:24:02 +0900

    sffdgsdfg

--- wasabi/assets/js/mobile.js
+++ wasabi/assets/js/mobile.js
@@ -1,8 +1,6 @@
-// Mobile여부를 구분하기 위함
-    var uAgent = navigator.userAgent.toLowerCase(); 
-
-    // 아래는 모바일 장치들의 모바일 페이지 접속을위한 스크립트
-    var mobilePhones = new Array('iphone', 'ipod', 'ipad', 'android', 'blackberry', 'windows ce','nokia', 'webos', 'opera mini', 'sonyericsson', 'opera mobi', 'iemobile');
-    for (var i = 0; i < mobilePhones.length; i++)
-        if (uAgent.indexOf(mobilePhones[i]) != -1)
-           document.location = "https://knmango.github.io/wasabi/mobile_wasabi";
+var filter = "win16|win32|win64|mac|macintel";
+if( navigator.platform  ){
+        if( filter.indexOf(navigator.platform.toLowerCase())<0 ){
+             location.href = "https://knmango.github.io";
+        }
+ }
